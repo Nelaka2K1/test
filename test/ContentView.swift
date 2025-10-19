6179cd989f96b230b90dd943bde551575a96f24a
N2k1 <n2k1@N2k1s-Mac.mshome.net>
2025-10-19 10:36:25 +0530

    tst

---
+++ test/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by N2k1 on 2025-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("moda charith")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by N2k1 on 2025-10-15.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
